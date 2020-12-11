5a96e43feaeb61ad1999d62dd25cfdacc7ecd044
Cael Warner <52902343+CaelWarner@users.noreply.github.com>
2020-12-11 01:21:24 -0800

    Solved day 10

--- src/com/caelwarner/adventofcode/twentytwenty/ten/AdapterArray.java
+++ src/com/caelwarner/adventofcode/twentytwenty/ten/AdapterArray.java
@@ -10,14 +10,14 @@ public class AdapterArray {
     public static void main(String[] args) {
         List<Integer> input = Read.asIntArray("adventofcode/twentytwenty/ten/input.txt");
 
-        System.out.println(findJoltage(input));
+        System.out.println(findPossibleSolutions(input));
     }
 
     private static int findJoltage(List<Integer> input) {
         Collections.sort(input);
 
-        int oneDifference = 0;
-        int threeDifference = 0;
+        int oneDifference = 1;
+        int threeDifference = 1;
 
         for (int i = 0; i < input.size() - 1; i++) {
             int difference = input.get(i + 1) - input.get(i);
@@ -29,10 +29,36 @@ public class AdapterArray {
             }
         }
 
-        System.out.println(oneDifference);
-        System.out.println(threeDifference);
-
         return oneDifference * threeDifference;
+    }
+
+    private static long findPossibleSolutions(List<Integer> input) {
+        Collections.sort(input);
+
+        input.add(0, 0);
+        input.add(input.get(input.size() - 1) + 3);
+
+        int[] multiplyList = {1, 1, 1, 2, 4, 7};
+        int currentLength = 0;
+        long combinations = 1;
+
+        for (int i = 0; i < input.size() - 1; i++) {
+            int difference = input.get(i + 1) - input.get(i);
+
+            currentLength++;
+
+            if (difference == 1) {
+
+            } else if (difference == 3) {
+                combinations *= multiplyList[currentLength];
+
+                currentLength = 0;
+            }
+        }
+
+        System.out.println(input);
+
+        return combinations;
     }
 
 }
