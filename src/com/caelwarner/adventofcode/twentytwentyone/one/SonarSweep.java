3572ab7080dd7b63463e945d6a1f73d6c0ea76d9
Cael Warner <52902343+CaelWarner@users.noreply.github.com>
2021-12-05 22:43:09 -0800

    Solved 2021 Day 1, 2 and 3

---
+++ src/com/caelwarner/adventofcode/twentytwentyone/one/SonarSweep.java
@@ -0,0 +1,47 @@
+package com.caelwarner.adventofcode.twentytwentyone.one;
+
+import com.caelwarner.util.Read;
+
+import java.util.List;
+
+public class SonarSweep {
+
+	public static void main(String[] args) {
+		List<Integer> input = Read.asIntArray("adventofcode/twentytwentyone/one/input.txt");
+
+		System.out.println(countDepthSumIncreases(input));
+	}
+
+	private static int countDepthIncreases(List<Integer> input) {
+		int previousDepth = 0;
+		int depthIncreases = 0;
+
+		for (int depth : input) {
+			if (depth > previousDepth) {
+				depthIncreases++;
+			}
+
+			previousDepth = depth;
+		}
+
+		return depthIncreases - 1;
+	}
+
+	private static int countDepthSumIncreases(List<Integer> input) {
+		int previousDepthSum = 0;
+		int depthSumIncreases = 0;
+
+		for (int i = 0; i < input.size() - 2; i++) {
+			int sum = input.get(i) + input.get(i + 1) + input.get(i + 2);
+
+			if (sum > previousDepthSum) {
+				depthSumIncreases++;
+			}
+
+			previousDepthSum = sum;
+		}
+
+		return depthSumIncreases - 1;
+	}
+
+}
